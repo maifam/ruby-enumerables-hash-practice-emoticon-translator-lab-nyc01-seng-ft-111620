f6fb7416860564c1ce414e68b65885f9bc1b233f
Mai Pham <maitphm@gmail.com>
2020-11-02 21:57:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(path)
+  
+  emoticons = YAML.load_file('lib.emoticons.yml')
+  
+  emoticon_list = {} 
+  
+  emoticons.each do |key, value|
+    emoticon_list[key] = {}
+    emoticon_list[key][:english] = value[0]
+    emoticon_list[key][:japanese] = value[1]}
+    binding.pry
+  end 
+  emoticon_list
+  
 end
 
 def get_japanese_emoticon
