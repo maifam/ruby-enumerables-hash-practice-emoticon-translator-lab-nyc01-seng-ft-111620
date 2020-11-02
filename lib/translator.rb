22306a6c5070501d7bea3fd9b741d2d0fa809e62
Mai Pham <maitphm@gmail.com>
2020-11-02 22:12:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   
   load_library(path).each do |key, value| 
     if value[:japanese] == emoticon 
