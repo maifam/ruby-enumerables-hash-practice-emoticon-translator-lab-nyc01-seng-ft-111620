86763b1ddd5f21ded1a18ae4a74bdd93241c59cf
Mai Pham <maitphm@gmail.com>
2020-11-02 22:31:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,11 @@
 
 require 'yaml'
 
-emoticons = YAML.load_file('lib/emoticons.yml')
+
   
 def load_library(file_path)
+  
+  emoticons = YAML.load_file('lib/emoticons.yml')
   
   emoticon_list = {} 
   
